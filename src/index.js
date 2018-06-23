337df7d43aff8ac4b7bfc43f45bfb5863ef4c119
Volkov Sergey <truefunrun@hotmail.com>
2018-06-23 04:09:24 +0300

    update styles

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import Prometey, { tag, element, find } from './prometey'
-import Input from './components/Input'
+import Prometey, { element } from './prometey'
+// import Input from './components/Input'
 import './styles.css'
-
-const logoSrc =
-  'http://www.dumpaday.com/wp-content/uploads/2018/06/00-94-620x280.jpg'
-
-// tag('super-input', (value, placeholder) => {
-//   return create('div.input-wrapper', [
-//     create('span.placeholder', placeholder),
-//     create('input.super-input', {
-//       input: () => console.log('it is changed'),
-//       value,
-//     }),
-//   ])
-// })
 
 Prometey.connect(
   element('#app->div.wrapper', [
     element('div.header', [
-      element('img.logo', logoSrc),
-      element('div.title', 'Dogs'),
+      element(
+        'img.logo',
+        'https://www.shareicon.net/data/512x512/2016/10/20/846441_alphabet-letter-letters-p-red_512x512.png'
+      ),
+      element('div.title', 'Prometey'),
     ]),
-    element(Input, { placeholder: 'default placeholder' }),
-    element('section.footer', [element('span', 'End of page')]),
+    element('div.content.pages', [
+      element('div.page.main-page'),
+      element('div.page.second-page'),
+      element('div.page.third-page'),
+      element('div.page.fourth-page'),
+      element('div.page.fifth-page'),
+      element('div.page.sixth-page'),
+      // element(Input, { placeholder: 'default placeholder' }),
+    ]),
   ])
 )
